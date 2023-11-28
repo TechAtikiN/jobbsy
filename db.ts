66c7401a4cbb4d7c18822b67b0c6f0918b6f5da1
techatikin <nikitakhabya03@gmail.com>
2023-11-28 16:00:41 +0530

    feat: minor change

--- db.ts
+++ db.ts
@@ -1,29 +1,11 @@
-// export const companies = [
-//   {
-//     name: 'Google',
-//     location: 'Mountain View, California',
-//     description: 'Google LLC is a multinational technology company that specializes in internet-related services and products. Google is widely known for its search engine but has expanded its offerings to include online advertising technologies, cloud computing, software, and hardware.',
-//     about: 'Founded in 1998 by Larry Page and Sergey Brin while they were Ph.D. students at Stanford University, Google has grown into one of the world\'s largest and most influential technology companies. Google\'s mission is to organize the world\'s information and make it universally accessible and useful.',
-//     benefits: 'Google places a strong emphasis on the well-being and happiness of its employees. Our benefits package includes competitive salaries, comprehensive health insurance, retirement plans, and generous paid time off. Google is committed to fostering a diverse and inclusive workplace, offering equal employment opportunities to all.',
-//     policies: 'Google is dedicated to maintaining a workplace that values integrity, transparency, and ethical conduct. Our policies promote a culture of innovation, collaboration, and respect. We prioritize user trust and privacy, implementing robust measures to safeguard user data. Google employees adhere to a code of conduct that reflects our commitment to ethical business practices.',
-//     website: 'https://www.google.com/'
-//   },
-//   {
-//     name: 'Microsoft',
-//     location: 'Redmond, Washington',
-//     description: 'Microsoft Corporation is a global technology company that develops, licenses, and sells a wide range of software, hardware, and services. Microsoft is known for its operating system, Windows, and a suite of productivity software, including Microsoft Office.',
-//     about: 'Founded in 1975 by Bill Gates and Paul Allen, Microsoft has played a significant role in the growth of the personal computer industry. With a mission to empower every person and every organization on the planet to achieve more, Microsoft continues to innovate in cloud computing, artificial intelligence, and other transformative technologies.',
-//     benefits: 'Microsoft is committed to the success and well-being of its employees. Our benefits package includes competitive salaries, comprehensive health coverage, retirement plans, and ample opportunities for professional development. We embrace diversity and inclusion, providing equal opportunities to all employees.',
-//     policies: 'Microsoft upholds a culture of integrity, accountability, and ethical behavior. Our policies promote a collaborative and inclusive work environment. We prioritize the privacy and security of user data, implementing robust measures to protect sensitive information. Microsoft employees adhere to a code of conduct that reflects our commitment to ethical business practices.',
-//     website: 'https://www.microsoft.com/'
-//   },
-//   {
-//     name: 'Amazon',
-//     location: 'Seattle, Washington',
-//     description: 'Amazon.com, Inc. is a multinational technology and e-commerce company that focuses on retail, cloud computing, digital streaming, artificial intelligence, and more. Amazon is one of the world\'s largest and most diverse technology companies, serving millions of customers globally.',
-//     about: 'Founded in 1994 by Jeff Bezos, Amazon started as an online bookstore and has since expanded its offerings to include a vast array of products and services. With a customer-centric approach and a commitment to innovation, Amazon has transformed the way people shop and consume digital content.',
-//     benefits: 'Amazon values the contributions of its diverse workforce. Our benefits package includes competitive compensation, health and wellness programs, retirement plans, and opportunities for career growth. We foster an inclusive and customer-obsessed culture.',
-//     policies: 'Amazon is dedicated to maintaining a workplace that values diversity, innovation, and customer obsession. Our policies promote a culture of continuous improvement and accountability. We prioritize the privacy and security of customer and employee data, implementing rigorous measures to ensure data protection. Amazon employees adhere to a code of conduct that reflects our commitment to customer trust and satisfaction.',
-//     website: 'https://www.amazon.com/'
-//   },
-// ];
+export const companies = [
+  {
+    name: 'Google',
+    location: 'Mountain View, California',
+    description: 'Google LLC is a multinational technology company that specializes in internet-related services and products. Google is widely known for its search engine but has expanded its offerings to include online advertising technologies, cloud computing, software, and hardware.',
+    about: 'Founded in 1998 by Larry Page and Sergey Brin while they were Ph.D. students at Stanford University, Google has grown into one of the world\'s largest and most influential technology companies. Google\'s mission is to organize the world\'s information and make it universally accessible and useful.',
+    benefits: 'Google places a strong emphasis on the well-being and happiness of its employees. Our benefits package includes competitive salaries, comprehensive health insurance, retirement plans, and generous paid time off. Google is committed to fostering a diverse and inclusive workplace, offering equal employment opportunities to all.',
+    policies: 'Google is dedicated to maintaining a workplace that values integrity, transparency, and ethical conduct. Our policies promote a culture of innovation, collaboration, and respect. We prioritize user trust and privacy, implementing robust measures to safeguard user data. Google employees adhere to a code of conduct that reflects our commitment to ethical business practices.',
+    website: 'https://www.google.com/'
+  }
+];
